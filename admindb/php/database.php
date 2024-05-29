a89a413478c1131b60f5fa5356c83e9e8c610806
Talha Javed <talhach1353@gmail.com>
2024-05-29 15:51:32 +0500

    testing github

--- admindb/php/database.php
+++ admindb/php/database.php
@@ -9,6 +9,7 @@ $database = mysqli_connect($servername, $username, $password, $db);
  
 if($database){
     echo"database is connected <br>";
+    echo"database is connected <br>";
 } else{
     echo"something went wrong";
 }
